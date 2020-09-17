92b0c7648492e8f57f905249267f4a30ea99d045
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-17 13:45:06 -0300

    atualização do Index

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,24 @@ import dotenv from "dotenv";
 import { AddressInfo } from "net";
 import express from "express";
 import userRouter from "./routes/userRouter";
+import imageRouter from "./routes/imageRouter";
 import cors from "cors";
 
 dotenv.config();
-//
+
 const app = express();
-//
+
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-//
 app.use("/users", userRouter);
-//
+app.use("/images", imageRouter);
+
 const server = app.listen(3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
   } else {
-    console.error(`Falha ao rodar o servidor`);
+    console.error(`Failed to run the server`);
   }
 });
