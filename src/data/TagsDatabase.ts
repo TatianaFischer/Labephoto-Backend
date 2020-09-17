9ee3bc4a9965f5615271e1a4cbd98307a02e1347
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-17 12:44:48 -0300

    TagsDatabase com função de pegar a tag pelo id e de inserir a tag na imagem

---
+++ src/data/TagsDatabase.ts
@@ -0,0 +1,30 @@
+import { BaseDatabase } from "./base/BaseDatabase";
+import { Image } from "../model/Image";
+
+export class TagsDatabase extends BaseDatabase {
+  public async getTagsById(name: string): Promise<Image> {
+    try {
+      const tags = await this.getConnection()
+        .select("id")
+        .from(this.tableNames.tags)
+        .where({ name });
+      //   console.log(tags[0]);
+      return Image.toImageModel(tags[0].id);
+    } catch (err) {
+      throw new Error(err.sqlMessage || err.message);
+    }
+  }
+
+  public async insertTagsToImage(imageId: string, tagId: string) {
+    try {
+      await this.getConnection()
+        .insert({
+          img_id: imageId,
+          tags_id: tagId,
+        })
+        .into(this.tableNames.imageWithTagsId);
+    } catch (err) {
+      throw new Error(err.sqlMessage || err.message);
+    }
+  }
+}
