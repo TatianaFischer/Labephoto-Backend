c5fe58712e256ac31d9ec2d30ce0eeb43f58eab9
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-30 19:18:32 -0300

    porta alternativa

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/images", imageRouter);
 
-const server = app.listen(3000, () => {
+const server = app.listen(3000 || 3001, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
